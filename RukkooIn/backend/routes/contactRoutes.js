3342ffe80c62305033cc6f7011fae5a587f8a2e7
Srthk-08 <sarthaknamdev2002@gmail.com>
2026-01-22 15:31:51 +0530

    push removed the files

--- RukkooIn/backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createContactMessage } from '../controllers/contactController.js';
-
-const router = express.Router();
-
-router.post('/:audience', createContactMessage);
-
-export default router;
-
--- RukkooIn/backend/routes/infoRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPublicPage, getPublicPlatformStatus } from '../controllers/infoController.js';
-
-const router = express.Router();
-
-router.get('/platform/status', getPublicPlatformStatus);
-router.get('/:audience/:slug', getPublicPage);
-
-export default router;
